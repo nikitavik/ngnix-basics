54c1d748975beb468bd1822e24e2be3124ab114e
Nikita Kornilov <n.kornilov@everypixel.com>
2025-11-22 06:46:06 +0600

    Conteinerize nginx as well

--- nginx-cert/openssl.bash
+++ nginx-cert/openssl.bash
@@ -1,1 +1,1 @@
-openssl req -x509 -nodes -days 5 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
